19f54c3b3955c6c6a3dca127af8875eb8d8f44bb
sandhya kumari <sandhyasingh950823256@gmail.com>
2026-09-17 15:50:28 +0530

    Add Features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import HeroSection from './sections/HeroSection.jsx'
+import About from './sections/About.jsx'
+import Stats from './sections/Stats.jsx'
+import Dishes from './sections/Dishes.jsx'
+import Features from './sections/Features.jsx'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <HeroSection/>
+      <About/>
+      <Stats/>
+      <Dishes/>
+      <Features/>
     </>
   )
 }
